09686f21b97c6d035c7db847da5c7ffadb3eb47e
dmitrypopov4912 <dmskppv@gmail.com>
2026-06-01 16:02:41 +0300

    full parse & insert to main.go

--- spots_parse.go
+++ spots_parse.go
@@ -1,12 +1,11 @@
 package main
 
 import(
-	"fmt"
 	"strings"
 )
 
 func CleanSpotName (name string) string{
-	googName := strings.TrimSpace(name)
+	goodName := strings.TrimSpace(name)
 	goodName = strings.ToLower(goodName)
 	goodName = strings.ReplaceAll(goodName, "#", "-")
 	return goodName
