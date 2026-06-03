66a9f13537ab84235939c3c47ac34f3ba6b1a275
dmitrypopov4912 <dmskppv@gmail.com>
2026-06-03 14:59:13 +0300

    add light and roof filter

---
+++ spot_light_and_roof.go
@@ -0,0 +1,15 @@
+package main
+
+func hasRoof(roof int16) bool{
+	if(roof == 1){
+		return true
+	}
+	return false
+}
+
+func hasLight(light int16) bool{
+	if(light == 1){
+		return true
+	}
+	return false
+}
